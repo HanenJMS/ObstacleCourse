d70a4854795dd98ad9c3e21191786fc7e3b00ad3
HanenJMS <61300039+HanenJMS@users.noreply.github.com>
2022-01-07 21:52:25 -0500

    Create OnHit.cs
    no particular change here. just uploading stuff.

---
+++ OnHit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
+        }
+            
+    }
+}
